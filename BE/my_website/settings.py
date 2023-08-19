052582535c8a51468d52df0eb981d24a33e30469
amrabrazek <amr.abdelrazek@wyz-solutions.com>
2023-08-19 16:58:26 +0300

    new updates for heroku deployment

--- BE/my_website/settings.py
+++ BE/my_website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!8fzrc1-v%m2u4!7fwy2dnxwj6jbm3knnw+ng!tekt)%glg@hc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
